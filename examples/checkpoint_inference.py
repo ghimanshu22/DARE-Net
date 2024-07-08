65cb5675d26a82948c88216189e8171b17c21527
ghimanshu22 <134205975+ghimanshu22@users.noreply.github.com>
2024-07-08 07:50:23 +0530

    Create checkpoint_inference.py

---
+++ examples/checkpoint_inference.py
@@ -0,0 +1,35 @@
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from models.lightning_model import *
+from datasets.reverb_speech_data import DareDataset
+from utils.utils import getConfig
+import matplotlib.pyplot as plt
+import argparse
+p = argparse.ArgumentParser()
+p.add_argument('ckpt_path')
+p.add_argument('config_path')
+
+def run_checkpoint(config_path,ckpt_path):
+    cfg = getConfig(config_path)
+
+    model = SpeechDAREUnet_v1.load_from_checkpoint(ckpt_path)
+    model.eval()
+
+    test_loader  = DareDataset(cfg,"test")
+    example = test_loader[0]
+    x = example[0]
+    y = example[1]
+    print(x)
+    prediction = model.predict(t.tensor(x[:,:,:,None],dtype=t.float).permute((0,3,1,2)))
+
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+    ax1.imshow(x[0,:,:].squeeze())
+    ax2.imshow(y[0,:,:].squeeze())
+    ax3.imshow(prediction[0,0,:,:].squeeze().detach().numpy())
+    plt.show()
+
+if __name__=='__main__':
+    args = p.parse_args()
+    run_checkpoint(**vars(args))
